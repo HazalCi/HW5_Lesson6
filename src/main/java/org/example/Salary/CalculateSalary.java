1ca4e174ecbe78da6f60ada4801e2491f8d7eade
HazalCi <hazalcigerim@gmail.com>
2024-06-05 10:24:15 +0300

    HW5

--- src/main/java/org/example/Salary/CalculateSalary.java
+++ src/main/java/org/example/Salary/CalculateSalary.java
@@ -1,15 +1,15 @@
 package org.example.Salary;
 
 public class CalculateSalary {
-    static double salary;
     public static void calculateMonthlySalary(Person person) {
-        if (person.workday <= 30) {
+        double salary;
+        if (person.workday < 0 || person.workday > 30) {
+            System.out.println("Lütfen 1 aylık çalışma günü giriniz.");
+
+        }  else {
             salary = person.workday * person.dailycharge;
             salary += person.workday > 25 ? (person.workday - 25) * 1000 : 0;
             System.out.println( "Personel İsmi: " + person.name +"\nPersonel No: " + person.no +"\nPersonel Aylık Çalışma Ücreti: " + salary +"TL");
-        }  else {
-
-            System.out.println("Lütfen 1 aylık çalışma günü giriniz.");
         }
     }
 }
